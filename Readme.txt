21d48bf77e9632f8ddf496e1a904861715ede474
jaimatadi <jaimatadi@jaimatadi-HCL-Desktop.(none)>
2014-04-07 12:21:11 +0530

    Readme added

---
+++ Readme.txt
@@ -0,0 +1,23 @@
+/*
+ * Assignment: Learning With Kernels
+ * Author: Sahil Kumar
+ */
+ /*****Important Note****/
+ 1) This program runs on octave versions >= 3.6.4. else saveas function fails.
+  
+ /****Running the program*******/
+ 1. Code is written in octave.
+ 2. "code.m" contains all the code.
+ 3. To run the code follow the following steps:
+ 	a) Open terminal.
+ 	b) Change the directory to the "code.m"
+ 	c) Type "octave". Press Enter.
+ 	d) To run the file type "code". Press enter.
+ 	e) To run the function, type the name of the function.
+ 	
+ /****Running the functions*******/
+ 1) In the file code.m there are instructions for running the various input files.
+ 2) You will have to comment and uncomment the lines in the code.m, to run for the iris.data or optdigit.data.
+ 3) You can also set the values of parameters(p and sigme) as defined in the file.
+ 4) After making the suitable changes in the octave script, you can follow "Running the program section". 	
+  
--- code.m
+++ code.m
@@ -1,21 +1,33 @@
 1;
 function [pdirection,pcomponent] = pca()
-  clear;
-  load optdigit.data;
-  iris = optdigit;
-  m = size(iris)(1);
-  n = size(iris)(2);
-  X = iris(1:m,1:n-1);
-  Y = iris(1:m,n:n);
+  % This is a function for doing the Principal Component Analysis
+	% pdirection : Principal Directions
+	% pcomponent : Principal components projected on the principal directions
+	
+	clear;
+	
+	% Uncomment for the iris.data
+  load iris.data;
+  data = iris;
+	
+	% Uncomment for the optdigit.data
+  %load optdigit.data;
+  %data = optdigit;
+  
+  m = size(data)(1);
+  n = size(data)(2);
+  X = data(1:m,1:n-1);
+  Y = data(1:m,n:n);
   X = bsxfun(@minus, X, mean(X));
   
   [U, S, V] = svd (X, 0);
   
-  %Part 1a
+  % Part 1a
   pdirection = V(:,1:2);
   pcomponent= U*S(:,1:2);
   
   % Part 1b
+  % Calculating the G and H matrices
   for i = [0,0.5,1]
     temp = diag(S).^i;
     G = U*diag(temp);
@@ -23,6 +35,8 @@ function [pdirection,pcomponent] = pca()
     H = (diag(temp)*V')';
     G = G(:,1:2);
     H = H(:,1:2);
+    
+    %Printing the Biplots
     figure;
     hold on;
     for j = [1:size(G)(1)]
@@ -37,18 +51,18 @@ function [pdirection,pcomponent] = pca()
       plot(temp1, temp2,"-");
     endfor
     
+    %Giving the file names 
     if i == 0
-      print "1.png";
+      print "pca_1.png";
     elseif i == 0.5
-      print "2.png";
+      print "pca_2.png";
     else
-      print "3.png";
+      print "pca_3.png";
     endif
   endfor
   
   %Part 1c
   VNEW = zeros(size(V));
-  
   for i = [1:size(V)(2)]
     h1= figure;
     hold on;
@@ -59,32 +73,35 @@ function [pdirection,pcomponent] = pca()
     endfor
     INDEX = [1:1:size(X)(1)];
     plot(INDEX,ERROR,"-");
-    filename =strcat('error_' ,int2str(i));
+    filename =strcat('pca_error_' ,int2str(i));
     saveas(h1, filename,'png');
   endfor
   
   %Part 1d
   VAR = diag(S).^2;;
   INDEX = [1:1:size(VAR)(1)];
+  
+  %Printing Figure
   h1 = figure;
   hold on;
   plot(INDEX, VAR',"-");
-  saveas(h1, "eigenVar",'png');
+  saveas(h1, "pca_eigen_variation",'png');
 endfunction
 
+function [pcomponent] = kpca_fun()
 % KPCA implementation
 % We cannot calculate the pdirections
+% Function returns the principal components projection
 
-function [pcomponent] = kpca_fun()
   clear;
   
   %uncomment for iris data set
-  %load iris.data;
-  %data = iris;
+  load iris.data;
+  data = iris;
   
   %uncomment for optdigit data set
-  load optdigit.data;
-  data = optdigit;
+  %load optdigit.data;
+  %data = optdigit;
     
   m = size(data)(1);
   n = size(data)(2);
@@ -92,17 +109,17 @@ function [pcomponent] = kpca_fun()
   Y = data(1:m,n:n);
   X = bsxfun(@minus, X, mean(X));
   
-  %taking p = 2
+  %We are taking the value of p = 2
   for i = [1:size(X)(1)]
     for j = [1:size(X)(1)]
-      %K(i,j) = poly_kernel(X(i,:),X(j,:),2); %uncomment for polynomial kernel, taking p = 2;
-      K(i,j) =  radial_kernel(X(i,:),X(j,:),2);     %uncomment for radial kernel, taking sigma = 2;
+      K(i,j) = poly_kernel(X(i,:),X(j,:),2); 		 		 %uncomment for polynomial kernel, taking p = 2;
+      %K(i,j) =  radial_kernel(X(i,:),X(j,:),2);     %uncomment for radial kernel, taking sigma = 2;
     endfor
   endfor
   
+  %Calculating the eigen values of Kernel Matrix
   [EVECTOR EVALUE]=eig(K);
   L = diag(EVALUE);
-  
   [sorted index]=sort(L,'descend');
     
   W = zeros(size(EVECTOR));
@@ -110,10 +127,13 @@ function [pcomponent] = kpca_fun()
     W(:,i) = EVECTOR(:,index(i));
   endfor
   
+  %Calculating the pcomponent
   Projected_matrix = K*W;
   pcomponent = Projected_matrix;
   x_val = Projected_matrix(:,1);
   y_val = Projected_matrix(:,2);
+  
+  %Plotting Figures
   figure;
   plot(x_val,y_val,"ro");
   print "kpca_pcomponent.png";
@@ -122,15 +142,8 @@ function [pcomponent] = kpca_fun()
   INDEX = [1:1:size(sorted)];
   figure;
   plot(INDEX,sorted',"b.-");
-  print "eigen_value_kpca.png";
+  print "kpca_eigen_variation.png";
 endfunction
-
-%Simple PCA Kernel Function
-% xi = data point (row vector) 
-% xj = data point (row vector)
-function val = pca_kernel(xi,xj)
-  val = xi*xj';
-end
 
 %Polynomial Kernel Function
 % xi = data point (row vector) 
@@ -138,7 +151,7 @@ end
 % p is parameter value 
 function val = poly_kernel(xi,xj,p)
   val = (xi*xj'+1)^p;
-end
+endfunction
 
 %Radial basis function kernel
 % xi = data point (row vector) 
@@ -151,4 +164,4 @@ function val = radial_kernel(xi,xj,sigma)
   end
   diff = xi-xj;
   val = exp(-(diff*diff')/(2*sigma*sigma));
-end
+endfunction
